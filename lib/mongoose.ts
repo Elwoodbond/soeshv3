8e0faa372a713b1fbf88a28f1e0404aa529871f9
Elwood Bond <elwood.bond@gmail.com>
2023-09-25 20:54:17 -0400

    more mongodb fixes

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,17 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-let isConnected = false; // variable to check if connection is already established
+let isConnected = false; // Variable to track the connection status
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true);
+  // Set strict query mode for Mongoose to prevent unknown field queries.
+  mongoose.set("strictQuery", true);
 
-    if(!process.env.MONGODB_URL) return console.log('MONGODB_URL not found');
-    if (isConnected) console.log('Aleady connected to MongoDB');
+  if (!process.env.MONGODB_URL) return console.log("Missing MongoDB URL");
 
-    try {
-        await mongoose.connect(process.env.MONGODB_URL);
-        isConnected = true;
-    } catch (err) {
-        console.log('Error connecting to MongoDB: ', err); 
-    }
+  // If the connection is already established, return without creating a new connection.
+  if (isConnected) {
+    console.log("MongoDB connection already established");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+
+    isConnected = true; // Set the connection status to true
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
 };
